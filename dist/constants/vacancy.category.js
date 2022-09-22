f3166b077d5e7942553da37bb337f94c8076f0c8
mac <mac@MacBook-Pro-de-mac.local>
2022-09-22 21:25:42 +0100

    add vercel config

--- dist/constants/vacancy.category.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = void 0;
-const categoryVacancy = ["Development & IT", "Design & Creative", "Sales & Marketing", "Writing & Translation", "Admin & Customer Support", "Finance & Accounting", "Engineering & Architecture", "Legal"];
-var _default = categoryVacancy;
-exports.default = _default;
